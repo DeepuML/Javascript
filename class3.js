4c4bda168a297ef98be914a91e09ffd03adc858e
DeepuML <mahakal123321@gmail.com>
2025-08-04 22:51:57 +0530

    Added new JavaScript files

---
+++ class3.js
@@ -0,0 +1,133 @@
+// let name = "Deepu"
+// console.log("This is my name:", name);
+// var a = 10;
+// console.log("This is the value:", a);
+// var isStudent = false;
+// console.log("This is status of student: ", isStudent);
+
+// function greet(FirstName, LastName) {
+//     console.log('Hello , ${FirstName} ${LastName} Welcome to my world');
+// }
+
+// greet('Geethu', 'Deepu');
+
+// function greet(FirstName, LastName) {
+//     console.log(`Hello, ${FirstName} ${LastName} Welcome to my world`);
+// }
+
+// greet('Geethu', 'Deepu');
+
+// function greet() {
+// 	// Function body goes here
+// /*
+// To hide suggestions (IntelliSense) in VS Code, you can press `Esc` when the suggestion popup appears.
+// To disable suggestions permanently, go to Settings (`Ctrl+,`), search for "suggest", and uncheck "Editor: Quick Suggestions".
+// */
+
+// function greet(Gender, FirstName) {
+//     if (Gender == "Male") {
+//         console.log(`Hello Mr. ${FirstName}`);
+//     }
+//     else {
+//         console.log(`Hello Mrs. ${FirstName}`);
+//     }
+// }
+
+// let gender = "Male";
+// greet(gender, "Deepu");
+
+
+// function count() {
+//     for (let i = 0; i <= 1000; i++) {
+//         console.log(i);
+//     }
+// }
+// count();
+
+// let arr = [12, 3, 4, 4, 5, 5];
+// console.log(arr[1]);
+// console.log("Printing the elements of the array:  ");
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// let arr = [12, 3, 4, 4, 5, 5];
+// console.log("Printing the biggest number: ")
+// let biggest = arr[0];
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > biggest) {
+//         biggest = arr[i];
+//     }
+// }
+
+// console.log(biggest);
+
+// let constcndj = true;
+// const firstname = "deepu";
+// console.log("Hello from before");
+
+// firstname = 1;
+// console.log("hello from after")
+
+// let a = 3;
+// let b = 29;
+
+// console.log(a + b);
+
+// let age = 11;
+// let canvote = age > 12; true or false
+
+// console.log(canvote);
+
+// function greet(name) {
+//     return "Hello, " + name;
+// }
+
+// let ans = greet("Deepu");
+// console.log(ans);
+
+// function sum(num1, num2) {
+//     return num1 + num2;
+// }
+
+// let ans = sum(20, 39);
+// console.log("The sum of 20 and 39 is:" ,ans);
+
+
+// let person = {
+//     name: "deepu",
+//     age: "22",
+//     isStudent: true
+// }
+
+// console.log("This is my name: " + person.name);
+// console.log("This is my name: " + person["name"]);
+// console.log("This is my age: " + person.age);
+// console.log("This is my age: " + person["age"]);
+// console.log("This is my status: " + person.isStudent);
+// console.log("This is my status: " + person["isStudent"]);
+
+// function greet(user) {
+//     return "Hello " + user.name + " having age is: " + user.age + " and status : " + user.isStudent;
+// }
+
+// let user = {
+//     name: "Deepu",
+//     age: "26",
+//     isStudent: true
+// }
+
+// let ans = greet(user);
+// console.log(ans);
+
+// let arrr = ["Deepu", 21, {
+//     name: "deepu",
+//     age: 19,
+//     cities: ["delhi", "nduej", "dejwhe", {
+//         country: "India",
+//         nation: "USA"
+//     }]
+// }];
+
+// console.log(arrr[2]);
+// console.log(arrr[2].cities);
---
+++ class4.js
@@ -0,0 +1,103 @@
+// // import the fs
+// const fs = require("fs");
+// // call the fs
+// const content = fs.readFileSync('1.txt', 'utf-8');
+// console.log(content)
+
+// var user = {
+//     name: "deepu",
+//     age: 21,
+//     calculate: function () {
+//         return 10;
+//     }
+// }
+
+// console.log(user.calculate());
+
+// const fs = require("fs");
+// const data1 = fs.readFileSync("1.txt", "utf-8");
+// console.log(data1);
+
+// const data2 = fs.readFileSync("2.txt", "utf-8");
+// console.log(data2);
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+// function divide(a, b) {
+//     return a / b;
+// }
+
+// const ans = sum(1, 2);
+// console.log(ans);
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// function divide(a, b) {
+//     return a / b;
+// }
+
+// function doOperation(a, b, op) {
+//     return op(a, b);
+// }
+
+// console.log(doOperation(1, 3, subtract));
+
+//  Asynchronous Program
+
+// const fs = require("fs")
+
+// function print(err, content) {
+//     console.log(content);
+// }
+
+// console.log("hello1");
+
+// fs.readFile("1.txt", "utf-8", print);
+
+// console.log("hello2");
+
+// fs.readFile("2.txt", "utf-8", print);
+
+// console.log("hello3");
+
+// console.log("Final Output");
+
+
+//  Input / Ouput bound task
+
+// function greet() {
+//     console.log("Hi, Hello how are you .")
+// }
+
+// console.log("Hi this is javascript");
+
+// setTimeout(greet, 1000);
+
+// console.log("DONE !");
+
+// let c = 1;
+// //  cpu bound task
+// for (let i=0; i < 100000; i++) {
+//     c = c + 1;
+// }
+// console.log("Very Heavy operation.")
+
