e3c301691bb89cbe08e6c054af53394f6482a4d1
DeepuML <mahakal123321@gmail.com>
2026-01-12 23:15:11 +0530

    Adding some new code

---
+++ array.js
@@ -0,0 +1,49 @@
+// let marks = [10, 20, 30, 40, 50];
+// console.log(marks);
+
+// let months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+// console.log(months);
+
+// let arr = ["Hello", 123, true, 1234.34, 5555];
+// console.log(arr);
+// console.log(typeof(arr));
+
+// console.log(arr.length);
+
+// arr[3] = 234;
+// arr.push("New Element");
+// arr.pop();
+// arr.unshift("First Element");
+// arr.shift();
+// arr.unshift("bye");
+// console.log(arr);
+
+// let arr = [10, 20, 30, 40, 50];
+// let arr1 = arr;
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let num of arr) {
+//   console.log(num);
+// }
+
+// arr1.push("hello");
+// console.log(arr);
+// console.log(arr1);
+
+// const arr = [10, 20, 30, 40, 50];
+
--- strings.js
+++ strings.js
@@ -1,7 +1,29 @@
-const str1 = "Hello, World!";
-const str2 = 'Javascript is real';
-const str3 = `Template literals are powerful`;
+// const str1 = "Hello, World!";
+// const str2 = "Javascript is real";
+// const day = 18;
+// const str3 = `Template literals are powerful ${day} times over!`;
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
 
-console.log(str1);
-console.log(str2);
-console.log(str3);
+// const str = `This is my string`;
+// const a = str.toLowerCase();
+// const b = str.toUpperCase();
+// const c = str.toLocaleUpperCase();
+// console.log(str);
+// console.log(a);
+// console.log(b);
+// // console.log(c);
+
+// const str = `this is the learning of strings learning  in js learning`;
+// // console.log(str.indexOf("learn"));
+// // console.log(str.lastIndexOf("learn"));
+// // console.log(str.indexOf("learn", 10));
+// // console.log(str.search("js"));
+
+// // console.log(str.includes("learning"));
+// console.log(str.slice(11, 15));
+
+const str1 = "heelo ";
+const str2 = "world";
+
