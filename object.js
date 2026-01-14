911fef3653ba26ed6449dafbefbef76a0c62255b
DeepuML <mahakal123321@gmail.com>
2026-01-15 02:14:47 +0530

    Adding some new code

---
+++ object.js
@@ -0,0 +1,94 @@
+// const user = {
+//   name: "Deepu",
+//   age: 23,
+//   address: "India",
+//   amount: 45444,
+// };
+// create
+// user.adhar = 9246732000134;
+
+// update
+// user.age = 233;
+// delete
+// delete user.address;
+// console.log(user);
+
+// const user = {
+//   name: "Deepu",
+//   age: 23,
+//   address: "India",
+//   amount: 45444,
+// };
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+// for (let keys in user) {
+//   console.log(keys, user[keys]);
+// }
+
+// const user = {
+//   name: "Deepu",
+//   age: 23,
+//   address: "India",
+//   amount: 45444,
+// };
+
+// const { age, name } = user;
+// console.log(age, name);
+
+// const user = {
+//   name: "Deepu",
+//   age: 23,
+//   phone: "7467876886",
+//   address: {
+//     nation: "India",
+//     State: "UP",
+//     city: "Bareiily",
+//   },
+// };
+
+// Shallow Copy using separater
+// const user2 = { ...user };
+// user2.name = "Deepak";
+// user2.address.city = "Delhi";
+// console.log(user);
+// console.log(user2);
+
+// const user2 = structuredClone(user);
+// user2.name = "Deepak";
+// user2.address.city = "London";
+
+// console.log(user);
+// console.log(user2);
+
+// console.log(user2);
+// console.log(user);
+// console.log(user2 === user);
+// console.log(user);
+// console.log(user2);
+// console.log(user === user2);
+// console.log(user);
+// console.log(user.name);
+// console.log(user.address);
+
+// console.log(user.address.city);
+
+// key string and symbol
+// const ab = Symbol("ID");
+
+// const user = {
+//   name: "Deepu",
+//   age: 23,
+//   phone: " 7467876886",
+//   address: {
+//     nation: "India",
+//     city: "Bareilly",
+//     state: "UP",
+//   },
+//   0: 100,
+//   2: "Mohan",
+// };
+
+// console.log(user[2]);
