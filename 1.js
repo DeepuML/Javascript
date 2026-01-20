7a6def46476d1a10eece159f0d64c704cf138828
DeepuML <mahakal123321@gmail.com>
2026-01-20 23:09:46 +0530

    Adding Some Code

---
+++ 1.js
@@ -0,0 +1,15 @@
+console.log("Hello this is me.");
+
+setTimeout(() => {
+  console.log("This is the first timeout.");
+}, 5000);
+
+setTimeout(() => {
+  console.log("This is the second timeoout.");
+}, 10000);
+
+setTimeout(() => {
+  console.log("This is the third timeout.");
+}, 3000);
+
+console.log("This is the end of the file .");
---
+++ callback_hell.js
@@ -0,0 +1,29 @@
+// zomato application
+
+function placeOrder(Callback) {
+  console.log("Your payment is in progress");
+  setTimeout(() => {
+    console.log("Your Payment is done.");
+    Callback();
+  }, 3000);
+}
+
+function OrderFood(Callback) {
+  setTimeout(() => {
+    console.log("Your order is preparation is done.");
+    Callback();
+  }, 3000);
+}
+
+function pickupOrder() {
+  console.log("Your food is ready for the pickup");
+  setTimeout(() => {
+    console.log("You order is on the way");
+  }, 3000);
+}
+
+placeOrder(function() {
+  OrderFood(function() {
+    pickupOrder();
+  });   
+});
