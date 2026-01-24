8372f93e9a1fcd088486f52de0a378aae87d0d38
DeepuML <mahakal123321@gmail.com>
2026-01-24 16:56:46 +0530

    Adding Async code

--- promise/promise.js
+++ promise/promise.js
@@ -48,24 +48,54 @@
 
 // console.log(j3);
 
-fetch("https://api.github.com/users")
-  .then((response) => {
-    if (!response.ok) {
-      new Error("Data is not found in the server.");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    const first = document.getElementById("first");
-    const img = document.createElement("img");
-    img.src = data[0].avatar_url;
-    first.appendChild(img);
-  })
-  .catch((error) => {
-    // console.log("The error is: " + error);
-    const first = document.getElementById("first");
-    const h2 = document.createElement("h2");
-    h2.innerText = "The error is: " + error;
-    first.appendChild(h2);
-  });
+// fetch("https://api.github.com/users")
+//   .then((response) => {
+//     if (!response.ok) {
+//       new Error("Data is not found in the server.");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     const first = document.getElementById("first");
+//     const img = document.createElement("img");
+//     img.src = data[0].avatar_url;
+//     first.appendChild(img);
+//   })
+//   .catch((error) => {
+//     // console.log("The error is: " + error);
+//     const first = document.getElementById("first");
+//     const h2 = document.createElement("h2");
+//     h2.innerText = "The error is: " + error;
+//     first.appendChild(h2);
+//   });
+
+// const p1 = new Promise((resolve, reject) => {
+//   resolve("Promise resolved successfully");
+// });
+
+// // console.log(p1);
+
+// p1.then((response) => {
+//   console.log(response);
+// });
+
+// const p1 = new Promise((resolve, reject) => {
+//   resolve("Promise resolved successfully");
+// });
+
+// p1.then((response) => {
+//   console.log(response);
+// }).catch((error) => {
+//   console.error(error);
+// });
+
+async function github() {
+  const response = await fetch("https://api.github.com/users");
+  const data = await response.json();
+  console.log(data);
+}
+
+github();
+
+console.log("Hello world");
